8668a24ec4726c37243a36a4f21bee197c45096b
FunKodeT <cooteybug@gmail.com>
2024-04-23 16:48:16 -0600

    0.2; Updated: app.js[MAINDIR\], README.txt[MAINDIR\];

--- app.js
+++ app.js
@@ -13,5 +13,5 @@ app.get('/hello',(req,res)=>(
 app.use('/api/v1/tasks',tasks);
 
 // Basic Server Start Setup
-const PORT = 3000;
+const PORT = process.env.PORT||3000;
 app.listen(PORT, console.log(`Server is listening on Port ${PORT}...`));
